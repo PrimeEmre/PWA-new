16c17907ca1014728a36806c9d8fdfcc346bce62
PrimeEmre <ynsemrgzl@gmail.com>
2025-11-30 20:18:52 -0500

    fixed the errors

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 
 // AI image generate API
-function genareteImg() {
+function generateImg() {
     imagePrompt = document.getElementById('image-generate').value;
     const apiUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(imagePrompt)}?width=1920&height=1080&model=flux`;
     console.log(apiUrl);
